06c1bcce5af0d374b2404ade099da43a0f217371
tomstation2 <league@WTS5.attlocal.net>
2018-02-02 16:59:25 -0800

    fortuneTeller, cardGame IP

--- src/level1_JonathanCornick/FortuneTeller.java
+++ src/level1_JonathanCornick/FortuneTeller.java
@@ -18,20 +18,21 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
 
     JFrame frame = new JFrame();
 
-    int frameWidth = 500;
-    int frameHeight = 400;
+    int frameWidth = 600;
+    int frameHeight = 460;
 
     FortuneTeller() throws Exception {
    	 // 1. Choose an image for your fortune teller and put it in your default package
-   	 fortuneTellerImage = ImageIO.read(getClass().getResource("fortune_teller.jpeg"));
+   	 fortuneTellerImage = ImageIO.read(getClass().getResource("fortune_teller.jpg"));
    	 // 2. Adjust the frameWidth and frameHeight variables to fit your image nicely (doesn’t need a new line of code)
    	 // 4. add a mouse listener to the frame
+   	 frame.addMouseListener(this);
 
     }
 
     static void begin() {
    	 // 3. Welcome the user. Give them a hint for the secret location.
-
+JOptionPane.showMessageDialog(null, "Welcome, the key is hidden in a dark and mysterious place.");
     }
 
     @Override
@@ -39,22 +40,49 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
    	 int mouseX = e.getX();
    	 int mouseY = e.getY();
    	 // 5. Print the mouseX variable
-
+System.out.println(mouseX+" "+mouseY);
    	 // 6. Add the mouseY variable to the previous line so that it prints out too (no new line)
    	 // 7. Adjust your secret location co-ordinates here:
-   	 int secretLocationX = 0;
-   	 int secretLocationY = 0;
+   	 int secretLocationX = 338;
+   	 int secretLocationY = 280;
    	 /** If the mouse co-ordinates and secret location are close, we'll let them ask a question. */
    	 if (areClose(mouseX, secretLocationX) && areClose(mouseY, secretLocationY)) {
    		 // 8. Get the user to enter a question for the fortune teller
-
+JOptionPane.showInputDialog("What knowledge do you seek");
    		 // 9. Find a spooky sound and put it in your default package (freesound.org)
    		 // AudioClip sound = JApplet.newAudioClip(getClass().getResource("spooky.aif"));
    		 // 10. Play the sound
-
+AudioClip sound = JApplet.newAudioClip(getClass().getResource("spooky.wav"));
+sound.play();
    		 // 11. Use the pause() method below to wait until your music has finished
-
+pause(14);
    		 // 12. Insert your completed Magic 8 ball recipe (http://bit.ly/Zdrf6d) here
+// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
+int r = new Random().nextInt(4);
+// 3. Print out this variable
+System.out.println(r);
+// 5. If the random number is 0
+if (r==0) {
+	JOptionPane.showMessageDialog(null, "Yes");
+}
+// -- tell the user "Yes"
+// 6. If the random number is 1
+if (r==1) {
+	JOptionPane.showMessageDialog(null, "No");
+}
+// -- tell the user "No"
+if (r==2) {
+	JOptionPane.showMessageDialog(null, "Mabye you should ask Google?");
+}
+// 7. If the random number is 2
+
+// -- tell the user "Maybe you should ask Google?"
+if (r==03) {
+	JOptionPane.showMessageDialog(null, "Madame Fortuneteller has no time for petty questions like that!!");
+}
+// 8. If the random number is 3
+
+// -- write your own answer
 
    	 }
 
