79d2ee270eb97ca49fcd3df5c71daf8bb14651b5
ianbrowne10 <league@WTS7.attlocal.net>
2018-05-11 16:58:54 -0700

    Candyman, Netflix IP

--- src/level1_JonathanCornick/Candyman.java
+++ src/level1_JonathanCornick/Candyman.java
@@ -2,9 +2,13 @@ package level1_JonathanCornick;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -14,12 +18,16 @@ import javax.swing.JPanel;
 
 public class Candyman implements ActionListener {
 	int i = 0;
+	static final int CREEPY = 0;
+	static final int SCREAM = 1;
+
 	public static void main(String[] args) {
 		Candyman candyman = new Candyman();
 		candyman.run();
 	}
+
 	public void run() {
-		
+
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		JButton button = new JButton();
@@ -30,6 +38,7 @@ public class Candyman implements ActionListener {
 		frame.setVisible(true);
 		frame.pack();
 	}
+
 	private void showPictureFromTheInternet(String imageUrl) {
 		try {
 			URL url = new URL(imageUrl);
@@ -43,14 +52,38 @@ public class Candyman implements ActionListener {
 			e.printStackTrace();
 		}
 	}
+
+	public static void playSound(int whichSound) {
+		File soundFile = null;
+		if (whichSound == CREEPY)
+			soundFile = new File("/Users/league/Desktop/level1_JonathanCornick/src/level1_JonathanCornick/SOUND.wav");
+		else if (whichSound == SCREAM)
+			soundFile = new File("/Users/league/Desktop/level1_JonathanCornick/src/level1_JonathanCornick/SOUND2.wav");
+		try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+			Clip clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+			Thread.sleep(3400);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		i+=1;
-		
-		if (i==5) {
-			showPictureFromTheInternet("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIhdTf91zYk5q71O8yjufUtdSe_f2Z_LVjv8n06UMXWevbJjSs_g");
+		i += 1;
+		if (i<5) {
+		playSound(1);
 		}
+		if (i == 5) {
+			showPictureFromTheInternet(
+					"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIhdTf91zYk5q71O8yjufUtdSe_f2Z_LVjv8n06UMXWevbJjSs_g");
+		playSound(0);
+		
+		}
+	
 	}
 
 }
